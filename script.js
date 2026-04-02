53d40489bbac9f61183be51360894f31ecc8f11d
daliH-4cls <daliah2rnndz10@gmail.com>
2026-04-02 16:27:32 +0000

    1st save

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log("Script starts here");
+
+function playGame() {
+    console.log("click");
+
+    // Start Level 1
+    createCircle();
+}
+
+function createCircle() {
+    // Create a div element for the circle
+    let circle = document.createElement("div");
+
+    // Set text to a number
+    circle.innerText = "1";
+
+    // Set styles
+    circle.style.backgroundColor = "white";
+    circle.style.width = "55px";
+    circle.style.height = "35px";
+
+    // Link to html
+    document.body.appendChild(circle);
+}
